08337c5e40f7834fefc314b17f53cda75c26c79f
LwinMoeAung <53222794+lwinmoeaung-springarts@users.noreply.github.com>
2024-01-05 10:10:58 +0900

    fix provider controller

--- app/Services/AuthServices/AuthService.php
+++ app/Services/AuthServices/AuthService.php
@@ -28,6 +28,7 @@ class AuthService
             [
                 'name' => $user->getName(),
                 'email' => $user->getEmail(),
+                'password' => bcrypt($user->getId()),
 
             ]
         );
